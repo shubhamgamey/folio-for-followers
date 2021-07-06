8d5e3cb4c13abcde78684bfbece22479d6dc5b88
Shubham Game <67473968+shubhamgamey@users.noreply.github.com>
2021-07-07 01:05:02 +0530

    Create Social.jsx

---
+++ src/pages/Social.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout"
+
+const NotFound = () => (
+  <Layout>
+    <div style={{ textAlign: `center` }}>
+      <h1>Social</h1>
+      <p>Your Social Profile</p>
+    </div>
+  </Layout>
+)
+
+export default NotFound
